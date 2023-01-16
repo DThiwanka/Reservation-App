08cc02eac200cb9dbfd673a651d024e88bf68156
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2023-01-17 00:31:28 +0530

    Property List

---
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './featuredProperties.css'
+
+const FeaturedProperties = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FeaturedProperties
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Featured from '../../components/featured/Featured'
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
 import Header from '../../components/header/Header'
 import NavBar from '../../components/navBar/NavBar'
+import PropertyList from '../../components/propertyList/PropertyList'
 import "./home.css"
 
 const Home = () => {
@@ -11,7 +13,10 @@ const Home = () => {
       <Header/>
       <div className="homeContainer">
         <Featured/>
- 
+        <h1 className="homeTitle">Browse By Property Type</h1>
+        <PropertyList/>
+        <h1 className="homeTitle">Home Guests Love</h1>
+        <FeaturedProperties/>
       </div>
     </div>
   )
