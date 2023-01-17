284a51adf75278b741834f46942e580fcf522bc3
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2023-01-17 16:13:35 +0530

    Footer Completed

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,12 +1,66 @@
-import React from 'react'
-import './featuredProperties.css'
+import "./featuredProperties.css";
 
 const FeaturedProperties = () => {
   return (
-    <div>
-      
+    <div className="fp">
+      <div className="fpItem">
+        <img
+          src="https://images.unsplash.com/photo-1559508551-44bff1de756b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotel Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://images.unsplash.com/photo-1619631428091-1eaa03c3bdf1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Comfort Suites Airport</span>
+        <span className="fpCity">Austin</span>
+        <span className="fpPrice">Starting from $140</span>
+        <div className="fpRating">
+          <button>9.3</button>
+          <span>Exceptional</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=449&q=80"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Four Seasons Hotel</span>
+        <span className="fpCity">Lisbon</span>
+        <span className="fpPrice">Starting from $99</span>
+        <div className="fpRating">
+          <button>8.8</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://images.unsplash.com/photo-1622396481608-de991f7727af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Hilton Garden Inn</span>
+        <span className="fpCity">Berlin</span>
+        <span className="fpPrice">Starting from $105</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FeaturedProperties
+export default FeaturedProperties;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+
+      <div className="fLists">
+        <ul className="fList">
+            <li className="fListItem">Countries</li>
+            <li className="fListItem">Regions</li>
+            <li className="fListItem">Cities</li>
+            <li className="fListItem">Districts</li>
+            <li className="fListItem">Airports</li>
+            <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+            <li className="fListItem">Countries</li>
+            <li className="fListItem">Regions</li>
+            <li className="fListItem">Cities</li>
+            <li className="fListItem">Districts</li>
+            <li className="fListItem">Airports</li>
+            <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+            <li className="fListItem">Countries</li>
+            <li className="fListItem">Regions</li>
+            <li className="fListItem">Cities</li>
+            <li className="fListItem">Districts</li>
+            <li className="fListItem">Airports</li>
+            <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+            <li className="fListItem">Countries</li>
+            <li className="fListItem">Regions</li>
+            <li className="fListItem">Cities</li>
+            <li className="fListItem">Districts</li>
+            <li className="fListItem">Airports</li>
+            <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+            <li className="fListItem">Countries</li>
+            <li className="fListItem">Regions</li>
+            <li className="fListItem">Cities</li>
+            <li className="fListItem">Districts</li>
+            <li className="fListItem">Airports</li>
+            <li className="fListItem">Hotels</li>
+        </ul>
+      </div>
+      <div className="fText">
+        Copyright @ 2023 Bookings.
+      </div>
+    </div>
+  )
+}
+
+export default Footer
