6eab96f4ce5712f642d3e636eeae204416a97464
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2023-01-18 23:58:29 +0530

    Updated

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -3,8 +3,19 @@ import './searchitem.css'
 
 const SearchItem = () => {
   return (
-    <div>
-      search item
+    <div className='searchItem'>
+      <img src="https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=449&q=80" alt="testjpg" className='siImage' />
+
+    <div className='siDesc'>
+      <h1 className='siTitle'>Tower Street Apartments</h1>
+      <span className='siDistance'>Free Airport Center</span>
+      <span className='siTaxiOp'>Free Airport Taxi</span>
+      <span className='siSubtitle'>Studio Aprtments with Air Conditioning</span>
+      <span className='siFeatures'>Entire Studio * 1 bathroom * 21m<sup>2</sup> 1 full bed</span>
+      <span className='siCancelOp'>Free Cancellation</span>
+      <span className='siCancelOpSubtitle'>You Can Cancel Later,<blockquote>So lock in this great price Today;</blockquote></span>
+    </div>
+    <div className='siDetails'>details</div>
     </div>
   )
 }
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -11,10 +11,10 @@ const List = () => {
 
   const location = useLocation();
 
-  const [destination,setDestination] = useState(location.state.destination)
-  const [date,setDate] = useState(location.state.date)
-  const [openDate,setOpenDate] = useState(false)
-  const [options,setOptions] = useState(location.state.options)
+  const [destination, setDestination] = useState(location.state.destination)
+  const [date, setDate] = useState(location.state.date)
+  const [openDate, setOpenDate] = useState(false)
+  const [options, setOptions] = useState(location.state.options)
   // console.log(location);
 
   return (
@@ -24,65 +24,67 @@ const List = () => {
       <div className="listContainer">
         <div className="listWrapper">
           <div className="listSearch">
-          <h1 className="lsTitle">Search</h1>
+            <h1 className="lsTitle">Search</h1>
 
-          <div className="lsItem">
-            <label>Destination</label>
-            <input type="text" placeholder={destination} />
+            <div className="lsItem">
+              <label>Destination</label>
+              <input type="text" placeholder={destination} />
+            </div>
+
+            <div className="lsItem">
+              <label>Check-in Date</label>
+              <span onClick={() => setOpenDate(!openDate)}>
+                {`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`}
+              </span>
+
+              {openDate && <DateRange onChange={item => setDate([item.selection])} minDate={new Date()} ranges={date} />}
+            </div>
+
+            <div className="lsItem">
+              <label>Options</label>
+              <div className="lsOptions">
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Min Price <small>Per Night</small></span>
+                  <input type="number" className="lsOptionInput" />
+                </div>
+
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Max Price <small>Per Night</small></span>
+                  <input type="number" className="lsOptionInput" />
+                </div>
+
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Adult</span>
+                  <input type="number" min={1} className="lsOptionInput" placeholder={options.adult} />
+                </div>
+
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Children</span>
+                  <input type="number" min={0} className="lsOptionInput" placeholder={options.children} />
+                </div>
+
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Room</span>
+                  <input type="number" min={1} className="lsOptionInput" placeholder={options.room} />
+                </div>
+
+              </div>
+            </div>
+            <button>Search</button>
           </div>
+          <div className="listResult">
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            </div>
 
-          <div className="lsItem">
-            <label>Check-in Date</label>
-           <span onClick={()=>setOpenDate(!openDate)}>
-            {`${format(date[0].startDate,"MM/dd/yyyy")} to ${format(date[0].endDate,"MM/dd/yyyy")}`}
-           </span>
-           
-           {openDate && <DateRange onChange={item=>setDate([item.selection])} minDate={new Date()} ranges={date}/>}
-          </div>
-
-          <div className="lsItem">
-            <label>Options</label>
-          <div className="lsOptions">
-           <div className="lsOptionItem">
-            <span className="lsOptionText">Min Price <small>Per Night</small></span>
-            <input type="number" className="lsOptionInput" />
-           </div>
-
-           <div className="lsOptionItem">
-            <span className="lsOptionText">Max Price <small>Per Night</small></span>
-            <input type="number" className="lsOptionInput" />
-           </div>
-
-           <div className="lsOptionItem">
-            <span className="lsOptionText">Adult</span>
-            <input type="number" min={1} className="lsOptionInput" placeholder={options.adult} />
-           </div>
-
-           <div className="lsOptionItem">
-            <span className="lsOptionText">Children</span>
-            <input type="number" min={0} className="lsOptionInput" placeholder={options.children}/>
-           </div>
-
-           <div className="lsOptionItem">
-            <span className="lsOptionText">Room</span>
-            <input type="number" min={1} className="lsOptionInput" placeholder={options.room}/>
-           </div>
-
-          </div>
-          </div>
-<button>Search</button>
-          </div>
-            <div className="listResult"></div>
-            <SearchItem/>
-            <SearchItem/>
-            <SearchItem/>
-            <SearchItem/>
-            <SearchItem/>
-            <SearchItem/>
-          </div>
-          
         </div>
-      
+
+      </div>
+
     </div>
   )
 }
