c257085fb2ce3baaf7f84b6a5e6f1d5dd3d1cf15
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2023-01-31 03:03:23 +0530

    Verification Completed

--- BackEnd/routes/hotels.js
+++ BackEnd/routes/hotels.js
@@ -2,15 +2,16 @@ import express from "express";
 import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controller/hotel.js";
 import Hotel from '../models/Hotel.js'
 import { CreateError } from "../utils/error.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 
 //CREATE
-router.post("/", createHotel)
+router.post("/",verifyAdmin, createHotel)
 
 //UPDATE
-router.put("/:id",updateHotel)
+router.put("/:id",verifyAdmin,updateHotel)
 
 //DELETE
 router.delete("/:id", deleteHotel)
--- BackEnd/routes/users.js
+++ BackEnd/routes/users.js
@@ -1,22 +1,36 @@
 import express from "express";
 import { deleteUser, getUser, getUsers, updateUser } from "../controller/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 // //CREATE
 // //router.post("/", createUser)
 
+// router.get("/checkauthentication",verifyToken,(req,res,next)=>{
+//     res.send("Hello user, You're Logged In")
+// })
+
+// router.get("/checkuser/:id",  verifyUser,(req,res,next)=>{
+//     res.send("Hello user, You're Logged In and you can delete your account")
+// })
+
+// router.get("/checkadmin/:id",  verifyAdmin,(req,res,next)=>{
+//     res.send("Hello Admin, You're Logged In and you can delete All accounts")
+// })
+
+
 //UPDATE
-router.put("/:id",updateUser)
+router.put("/:id", verifyUser, updateUser)
 
 //DELETE
-router.delete("/:id", deleteUser)
+router.delete("/:id", verifyUser, deleteUser)
 
 //GET Details
-router.get("/:id", getUser)
+router.get("/:id", verifyUser, getUser)
 
 //GET ALL
-router.get("/",getUsers)
+router.get("/", verifyAdmin, getUsers)
 
 
 export default router;
